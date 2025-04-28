7dad62e34cae2b4e6a5e11fa2304e1beb2896673
Jathavaan Shankarr <jathavaan12@gmail.com>
2025-04-28 10:37:14 +0900

    chore: #3 Added MariaDB consumer and logic in entrypoint layer

--- src/config.py
+++ src/config.py
@@ -8,3 +8,7 @@ load_dotenv()
 
 class Config(Enum):
     SQLALCHEMY_DATABASE_URI = f"mariadb+mariadbconnector://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+
+    KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS')
+    KAFKA_GROUP_ID = "mariadb_consumer_group"
+    KAFKA_TOPIC = "game_topic"
---
+++ src/entrypoints/consumer.py
@@ -0,0 +1,38 @@
+﻿import logging
+
+from confluent_kafka import Consumer
+
+from src import Config
+from src.application import Container
+
+
+class MariaDbConsumer:
+    __consumer: Consumer
+    __logger: logging.Logger
+
+    def __init__(self):
+        container = Container()
+        self.__logger = container.logger()
+        self.__consumer = Consumer({
+            "bootstrap.servers": Config.KAFKA_BOOTSTRAP_SERVERS.value,
+            "group.id": Config.KAFKA_GROUP_ID.value,
+            "auto.offset.reset": "earliest",
+            "enable.auto.commit": True
+        })
+
+    def consume(self) -> None:
+        while True:
+            message = self.__consumer.poll(1.0)
+
+            if not message:
+                continue
+
+            if message.error():
+                self.__logger.error(message.error())
+                continue
+
+            self.__logger.info(message.as_string())
+
+    def close(self) -> None:
+        self.__logger.info("Shutting down MariaDB consumer")
+        self.__consumer.close()
