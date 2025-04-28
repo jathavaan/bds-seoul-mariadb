5fddf4ca67789626b628b7349a9b1af968c1f1b8
Jathavaan Shankarr <jathavaan12@gmail.com>
2025-04-28 09:36:23 +0900

    chore: #3 Configured alembic

---
+++ src/config.py
@@ -0,0 +1,10 @@
+﻿import os
+from enum import Enum
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config(Enum):
+    SQLALCHEMY_DATABASE_URI = f"mariadb+mariadbconnector://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
