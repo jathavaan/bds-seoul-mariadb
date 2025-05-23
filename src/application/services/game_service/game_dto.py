017cc857c5050ab9f07b65165ca33fc02e07191b
Jathavaan Shankarr <jathavaan12@gmail.com>
2025-05-23 16:41:01 +0900

    chore: #10 Added correlation ID for maria db messages

--- src/application/services/game_service/game_dto.py
+++ src/application/services/game_service/game_dto.py
@@ -10,17 +10,20 @@ class GameDto:
 @dataclass
 class LastScrapedDateRequestDto:
     game_id: int
+    correlation_id: str
 
 
 @dataclass
 class LastScrapedDateResponseDto:
     steam_game_id: int
     last_scraped_date: datetime | None
+    correlation_id: str
 
     def to_dict(self) -> dict:
         return {
             "steam_game_id": self.steam_game_id,
             "last_scraped_date": self.last_scraped_date.strftime(
                 "%Y-%m-%d-%H-%M-%S"
-            ) if self.last_scraped_date else None
+            ) if self.last_scraped_date else None,
+            "correlation_id": self.correlation_id
         }
